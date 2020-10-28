890e449999d5557ef33b8c997f7f4e5fffa3263e
garyng <garyng.zhongbo@gmail.com>
2020-10-29 03:17:03 +0800

    fix: disable mouse key

--- keyboards/converter/usb_usb/keymaps/garyng/rules.mk
+++ keyboards/converter/usb_usb/keymaps/garyng/rules.mk
@@ -7,7 +7,7 @@ LTO_ENABLE = yes # reduces the compiled firmware size
 # TAP_DANCE_ENABLE = yes
 
 EXTRAKEY_ENABLE = yes
-MOUSEKEY_ENABLE = yes
+MOUSEKEY_ENABLE = no
 CONSOLE_ENABLE = yes
 COMMAND_ENABLE = no
 NKRO_ENABLE = no
