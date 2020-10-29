7cf10dff30ae309ee31fa3730c8249e98dac4988
garyng <garyng.zhongbo@gmail.com>
2020-10-30 02:06:37 +0800

    feat: disable unwanted features

--- keyboards/converter/usb_usb/keymaps/garyng/config.h
+++ keyboards/converter/usb_usb/keymaps/garyng/config.h
@@ -2,4 +2,10 @@
 
 // place overrides here
 
-// #define NO_ACTION_ONESHOT
+// #define NO_DEBUG
+// #define NO_PRINT
+#define NO_ACTION_LAYER
+#define NO_ACTION_TAPPING
+#define NO_ACTION_ONESHOT
+// #define TAPPING_TERM 300
+// #define MAX_TAM_TAPS 3
