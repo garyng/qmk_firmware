57b47ab1bb3d3226c17158f3bbe562eee0dd3090
garyng <garyng.zhongbo@gmail.com>
2021-02-13 03:00:17 +0800

    misc: change product id and name

--- keyboards/converter/usb_usb/teensy_2pp/keymaps/garyng/config.h
+++ keyboards/converter/usb_usb/teensy_2pp/keymaps/garyng/config.h
@@ -7,3 +7,19 @@
 #define NO_ACTION_LAYER
 #define NO_ACTION_TAPPING
 #define NO_ACTION_ONESHOT
+
+#ifdef PRODUCT_ID
+#undef PRODUCT_ID
+#define PRODUCT_ID      0xBBBB
+#endif
+
+#ifdef DEVICE_VER
+#undef DEVICE_VER
+#define DEVICE_VER      0x0001
+#endif
+
+#ifdef PRODUCT
+#undef PRODUCT
+#define PRODUCT         Gary U2U Converter
+#endif
+
